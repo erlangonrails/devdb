e44dac9128a752c39fda230eb87e335504e6be4d
erlangonrails <erlangonrails@gmail.com>
2010-06-29 17:43:11 +0800

    增加了ejabberd关于好友推荐部分的模块(未完成, 只是完成了网络接口, 后台逻辑未实现)

---
+++ sandbox/ejabberd-2.1.4/src/mod_youbao_recommend.erl
@@ -0,0 +1,137 @@
+%%%-------------------------------------------------------------------
+%%% File    : mod_youbao_recommend.erl
+%%% Author  : liqiang 
+%%% Description : 
+%%%
+%%% Created : 29 Jun 2010 by liqiang 
+%%%-------------------------------------------------------------------
+-module(mod_youbao_recommend).
+
+-behaviour(gen_server).
+-behaviour(gen_mod).
+
+-define(PROCNAME, ejabberd_mod_youbao_recommend).
+-include("ejabberd.hrl").
+-include("jlib.hrl").
+
+%% API
+-export([start_link/2, start/2, stop/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-record(state, {host}).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link(Host, Opts) ->
+    Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+    gen_server:start_link({local, Proc}, ?MODULE, [Host, Opts], []).
+
+start(Host, Opts) ->
+    Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+    ChildSpec =
+	{Proc,
+	 {?MODULE, start_link, [Host, Opts]},
+	 temporary,
+	 1000,
+	 worker,
+	 [?MODULE]},
+    supervisor:start_child(ejabberd_sup, ChildSpec).
+
+stop(Host) ->
+    Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+    gen_server:call(Proc, stop),
+    supervisor:terminate_child(ejabberd_sup, Proc),
+    supervisor:delete_child(ejabberd_sup, Proc).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([Host, Opts]) ->
+    MyHost = gen_mod:get_opt_host(Host, Opts, "recommend.@HOST@"),
+    ejabberd_router:register_route(MyHost),
+    {ok, #state{host = MyHost}}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(stop, _From, State) ->
+    {stop, normal, ok, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info({route, From, To, Packet}, State) ->
+    Packet2 = case From#jid.user of
+		  "" ->
+		      jlib:make_error_reply(Packet, ?ERR_BAD_REQUEST);
+		  _ ->
+		      Users = <<"recommend-list">>, %% TODO:
+		      ID = xml:get_tag_attr_s("id", Packet),
+		      NewPacket = {xmlelement,"iq", [{"type","result"},
+                                                     {"to", jlib:jid_to_string(From)},
+                                                     {"from",jlib:jid_to_string(To)},
+                                                     {"id",ID}],
+                                                    [{xmlelement,"query",[{"xmlns","youbao:xmpp:recommend"}],
+						      [{xmlelement, "recommend", [],[{xmlcdata, Users}]}]}]},
+		      NewPacket
+               end,
+    ejabberd_router:route(To, From, Packet2),
+    {noreply, State};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, State) ->
+    ejabberd_router:unregister_route(State#state.host),
+    ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
