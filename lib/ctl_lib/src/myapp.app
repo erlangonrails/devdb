aa05bbd44f8cedbde5c719b33f099d6c9072fa72
erlangonrails <erlangonrails@gmail.com>
2010-07-07 16:45:53 +0800

    新增了一个erlang控制台和linux shell结合的项目版本ctl_lib, 这个项目以后可以用于其它项目的模板使用

---
+++ lib/ctl_lib/src/myapp.app
@@ -0,0 +1,8 @@
+{application, myapp,
+  [{description,
+    "myapp description"},
+   {vsn,          "1.0.0"},
+   {modules,      [myapp]},
+   {registered,   []},
+   {applications, [kernel, stdlib]},
+   {mod, {myapp,[]}}]}.
---
+++ lib/ctl_lib/src/myapp.erl
@@ -0,0 +1,25 @@
+-module(myapp).
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+-export([start/0, loop/0]).
+
+start() ->
+    application:start(myapp).
+
+start(normal, _StartArgs) ->
+    case proc_lib:spawn_link(?MODULE, loop, []) of
+	Pid -> 
+	    {ok, Pid}
+    end.
+
+stop(_State) ->
+    ok.
+
+loop() ->
+    timer:sleep(1000 * 5),
+    io:format("myapp is running...~n", []),
+    loop().
+
+
