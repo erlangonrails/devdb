6b363ea2fc6cd56607d05f5eabebdb46b1af77ad
erlangonrails <erlangonrails@gmail.com>
2010-07-20 11:45:43 +0800

    暂时删除了tcpbalance这个项目

--- sandbox/tcpbalance/src/balance.hrl
+++
@@ -1,8 +0,0 @@
-%% Name of the balancer application
--define(BALANCER_APP, balance).
-
-%% Module name for the TCP proxy
--define(TCPPROXY, tcp_proxy).
-
-%% Atom used to inform tcp_proxy proc that no backends are available.
--define(TIMEOUT_BE, timeout_be).
