7058e1ddfe5fccef8512b14ecaa9acd4de4ae962
erlangonrails <erlangonrails@gmail.com>
2010-08-24 10:15:24 +0800

    更新了erl-crontab, 使它支持以标准linux服务的形式运行

--- sandbox/erl-crontab/install.sh
+++ sandbox/erl-crontab/install.sh
@@ -6,9 +6,10 @@ ROOT="`dirname "$0"`"
 if [ $# -ne 1 ]; then
   echo "Usage install.sh Path" 
 else
-  mkdir $1
+  mkdir $INSTALLPATH
+  mkdir $INSTALLPATH/bin
 
-  cp -r $ROOT/bin $INSTALLPATH
+  cp -r $ROOT/bin/crontabctl $INSTALLPATH/bin
   cp -r $ROOT/ebin $INSTALLPATH
   cp -r $ROOT/etc $INSTALLPATH
   cp -r $ROOT/log $INSTALLPATH
